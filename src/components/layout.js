1c0354ce865e633c6562a60d5e11db8344b96cfe
lilybarrett <lilydbarrett@gmail.com>
2019-03-25 22:54:42 -0400

    add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -42,6 +42,7 @@ const Layout = ({ children }) => (
           <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet" />
           <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+          <link rel="icon" href="https://target.scene7.com/is/image/Target/GUEST_abef9ab3-3c6b-44fb-93c0-03421d838f5f?wid=488&hei=488&fmt=pjpeg"></link>
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <Navbar>
