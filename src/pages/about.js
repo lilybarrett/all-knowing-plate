5baf0b37713bdbf8566c26075ad4e78e0b2bf327
lilybarrett <lilydbarrett@gmail.com>
2019-03-25 22:32:18 -0400

    styling stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,22 +7,26 @@ const HighlightedPhrase = styled("span")`
     color: orange;
 `;
 
+const LineItem = styled("li")`
+    margin-bottom: 15px;
+`;
+
 const AboutPage = () => (
     <Layout>
         <p>Bad cook? <HighlightedPhrase>No problem!</HighlightedPhrase> Serve meals on these plates and you'll leave your guests raving about your skill. <HighlightedPhrase>Be our guest!</HighlightedPhrase></p>
         <ul>
-            <li>
+            <LineItem>
                 Use <HighlightedPhrase>visual cues</HighlightedPhrase>, <HighlightedPhrase>sensory hints</HighlightedPhrase>, and the <HighlightedPhrase>rate of a guest's chewing</HighlightedPhrase> to discern responses to food
-            </li>
-            <li>
+            </LineItem>
+            <LineItem>
                 Based on what the guest craves, enhances food's <HighlightedPhrase>sweetness,</HighlightedPhrase> <HighlightedPhrase>saltiness</HighlightedPhrase>, <HighlightedPhrase>sourness</HighlightedPhrase>, <HighlightedPhrase>umami</HighlightedPhrase>, or other flavors detected as desired 
-            </li>
-            <li>
+            </LineItem>
+            <LineItem>
                 <HighlightedPhrase>Deletes data</HighlightedPhrase> when they sense the guest is done with his or her meal.  The All-Knowing Plate does not store any private information about guests' food preferences.
-            </li>
-             <li>
+            </LineItem>
+             <LineItem>
                 <HighlightedPhrase>Self-cleaning</HighlightedPhrase>
-            </li>
+            </LineItem>
         </ul>
         <Image filename="spoons.jpg" />
     </Layout>
